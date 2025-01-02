f1e74a40440ee5d7a045152e30bf8c8a11348c28
raghumanimehta <rmehta07@students.cs.ubc.ca>
2025-01-03 01:31:52 +0530

    first commit- setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Intro from './components/intro'
+
+function App() {
+    return (<Intro />);
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Intro() {
+    return (<div>
+        <p>
+            Hello! I'm <span className="highlight">Raghumani Mehta</span>, a Computer Science student at UBC 
+            with a passion for both software development and artificial intelligence. My projects range from 
+            encrypted chat systems to intelligent applications, leveraging my expertise in Java, C++, and TypeScript.
+        </p>
+        <p>
+            While I've built secure and scalable full-stack solutions, I'm particularly excited about exploring 
+            the intersection of traditional software development and AI. I'm constantly learning and seeking 
+            opportunities to combine these fields to create more intelligent and impactful applications.
+        </p>
+    </div>);
+}
+
+export default Intro;
